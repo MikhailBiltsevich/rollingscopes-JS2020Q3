ad0e84eea8dc3ad40a1347a486a2e6d4d9788ad6
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-10-22 20:58:39 +0300

    implement: add links

--- shelter/pages/pets/index.js
+++ shelter/pages/pets/index.js
@@ -2,3 +2,17 @@ document.querySelector('.menu-icon-wrapper').onclick = function() {
     document.querySelector('.menu-icon').classList.toggle("menu-icon_active");
     document.querySelector('.menu').classList.toggle('menu_hidden');
 }
+
+function activeLinkClick() {
+    window.scrollTo(0, 0);
+    if(document.querySelector(".menu_active")) {
+        toggleMenu();
+    }
+}
+
+function redirectToMainPage() {
+    window.location.href = "/pages/main/index.html";
+}
+
+document.querySelector(".menu__link_active").addEventListener("click", activeLinkClick);
+document.querySelector(".logo").addEventListener("click", redirectToMainPage);
