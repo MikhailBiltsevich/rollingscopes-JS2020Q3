f5999c18716e730d22278bef1d5ce9313e84c8a0
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-10-25 23:00:07 +0300

    feat: implement momentum (add adaptive and weather)

--- momentum/script.js
+++ momentum/script.js
@@ -5,6 +5,12 @@ const focus = document.querySelector(".focus");
 const updateButton = document.querySelector(".update-image-button");
 const blockquote = document.querySelector(".blockquote");
 const refreshButton = document.querySelector(".refresh-button");
+const submitButton = document.querySelector(".forecast-form__submit");
+
+const forecastForm = document.querySelector(".forecast-form");
+const forecastInput = document.querySelector(".forecast-form__input");
+const currentWeatherInfo = document.querySelector(".current-weather-info");
+const forecastInfo = document.querySelector(".forecast-info");
 
 const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg'];
 let indexImage = Math.floor(Math.random() * images.length);
@@ -49,7 +55,6 @@ function setGreeting(){
 
   if (hours < 6) {
     greetingText = "Good night, ";
-    document.body.style.color = "white";
     imageDirectory = "./assets/images/night";
     setBackgroundImage();
   } else if (hours < 12) {
@@ -64,7 +69,6 @@ function setGreeting(){
     greetingText = "Good evening, ";
     imageDirectory = "./assets/images/evening";
     setBackgroundImage();
-    document.body.style.color = "white";
   }
 
   greeting.textContent = greetingText;
@@ -153,15 +157,93 @@ async function getAdvice() {
   refreshButton.style.animation = "";
 }
 
+async function getForecast(e) {
+  if (e !== undefined) {
+    e.preventDefault();
+  }
+
+  if(forecastInput.value === "") {
+    return;
+  }
+
+  submitButton.style.animation = "1s linear infinite rotation";
+  try {
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=8ae0c5c8cbc14f1fa32163939202510&q=${forecastInput.value}&days=3`);
+    if(response.status === 400) {
+      throw new BadRequest();
+    }
+    const forecast = (await response.json());
+  
+    currentWeatherInfo.innerHTML = `
+      <img src="${forecast.current.condition.icon}">
+      <div>${forecast.current.condition.text}</div>
+      <div>${forecast.current.temp_c} &deg;C</div>
+      <div>${forecast.current.wind_dir} ${forecast.current.wind_kph} kph</div>
+      <div>${forecast.current.humidity}%</div>`;
+  
+    forecastHTMLText = "";
+    for(let i = 0; i < forecast.forecast.forecastday.length; i++) {
+      let forecastday = forecast.forecast.forecastday[i].day;
+      let date = new Date(forecast.forecast.forecastday[i].date);
+  
+      forecastHTMLText += `
+        <div class="forecast-day-info">
+          <div>${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}</div>
+          <img src="${forecastday.condition.icon}">
+          <div>${forecastday.condition.text}</div>
+          <div>${forecastday.avgtemp_c} &deg;C</div>
+          <div>${forecastday.maxwind_kph} kph</div>
+          <div>${forecastday.avghumidity}%</div>
+        </div>`;
+
+      forecastInfo.innerHTML = forecastHTMLText;
+    }
+  } catch (error) {
+    let popup = document.createElement("div");
+    popup.classList.add("popup");
+    popup.textContent = "Error in getting weather request. Please make sure the city name is correct or try again later.";
+    document.body.append(popup);
+    setTimeout(() => { popup.remove(); }, 5000);
+  } finally {
+    submitButton.style.animation = "";
+  }
+
+}
+
+function getCity() {
+  if (localStorage.getItem("city") !== null) {
+    forecastInput.value = localStorage.getItem("city");
+  }
+}
+
+function setCity(e) {
+  if (e.type === "focus") {
+    forecastInput.value = "";
+  } else if (e.type === "blur") {
+    if (forecastInput.value.trim() !== "") {
+      localStorage.setItem("city", forecastInput.value);
+    } else {
+      forecastInput.value = "";
+    }
+
+    getCity();
+  }
+}
+
 name.addEventListener("focus", setName);
 name.addEventListener("blur", setName);
 focus.addEventListener("focus", setFocus);
 focus.addEventListener("blur", setFocus);
+forecastInput.addEventListener("focus", setCity);
+forecastInput.addEventListener("blur", setCity);
 updateButton.addEventListener("click", setBackgroundImage);
 refreshButton.addEventListener("click", getAdvice);
+forecastForm.addEventListener("submit", getForecast);
 
 getAdvice();
 setTime();
 setGreeting();
 getName();
 getFocus();
+getCity();
+getForecast();
