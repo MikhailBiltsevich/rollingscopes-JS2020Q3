bca671248a78153ded1192eff3bfe7b855e1eb10
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-12-01 21:46:36 +0300

    feat: add container of levels

---
+++ rs-css/src/levels.js
@@ -0,0 +1,13 @@
+export const Levels = [
+  {
+    id: 1,
+    title: 'Select all',
+    description: 'You have to select all landmarks',
+    selector: '*',
+    code: `
+        <landmark id="cremlin" />
+        <landmark id="louvre" />
+        <landmark id="pisa-tower" />
+      `
+  }
+];
