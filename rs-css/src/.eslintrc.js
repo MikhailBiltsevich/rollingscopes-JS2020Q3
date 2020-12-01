b3a4fcbfe2d3b35c3dea63e4d38f60f68d24f971
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-12-01 21:29:06 +0300

    feat: add eslint

---
+++ rs-css/src/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: 'airbnb-base/legacy',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  }
+};
