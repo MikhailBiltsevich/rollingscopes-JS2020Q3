a8564d81b11d9b3539f7535018f230949c3af9d7
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-12-03 18:32:36 +0300

    feat: add first loading view

--- rs-css/src/controller.js
+++ rs-css/src/controller.js
@@ -2,5 +2,19 @@ export default class Controller {
   constructor(view, model) {
     this.view = view;
     this.model = model;
+
+    this.onInitialLoad(this.model.levels);
+    this.view.bindCheckAnswer(this.handleCheckAnswer.bind(this));
+  }
+
+  onInitialLoad(levels) {
+    this.view.fillLevelsList(levels);
+    const level = this.model.getCurrentLevel();
+    this.view.setLevel(level);
+  }
+
+  handleCheckAnswer(selector) {
+    const level = this.model.getCurrentLevel();
+    console.log(selector === level.selector);
   }
 }
