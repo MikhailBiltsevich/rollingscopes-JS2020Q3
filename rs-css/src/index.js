ad7c89577d28406c5c66985af97ebe7b7c1afdd0
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-12-07 21:35:16 +0300

    feat: implement entry point of app

--- rs-css/src/index.js
+++ rs-css/src/index.js
@@ -1,1 +1,7 @@
 import './style.scss';
+import View from './view';
+import Model from './model';
+import Controller from './controller';
+
+// eslint-disable-next-line no-unused-vars
+const app = new Controller(new View(), new Model());
