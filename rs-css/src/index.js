9d83cdef2bcb67f6d35b075cd3292cbc04f7ac7d
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-12-16 16:43:51 +0300

    refactor: remove unused variable 'app'

--- rs-css/src/index.js
+++ rs-css/src/index.js
@@ -3,5 +3,5 @@ import View from './view';
 import Model from './model';
 import Controller from './controller';
 
-// eslint-disable-next-line no-unused-vars
-const app = new Controller(new View(), new Model());
+// eslint-disable-next-line no-new
+new Controller(new View(), new Model());
