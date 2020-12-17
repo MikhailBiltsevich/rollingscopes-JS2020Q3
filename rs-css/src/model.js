9b44e13e5c33ce5a385b50121d73eb93fb977cfa
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-12-17 17:56:23 +0300

    refactor: move completed levels marking logic  into model

--- rs-css/src/model.js
+++ rs-css/src/model.js
@@ -5,6 +5,16 @@ export default class Model {
   constructor() {
     this.levels = Levels;
     this.remoteStorage = LocalStorageWrapper;
+
+    this.markCompletedLevels();
+  }
+
+  markCompletedLevels() {
+    const completedLevels = this.getCompletedLevels();
+    for (let i = 0; i < this.levels.length; i += 1) {
+      const level = this.levels[i];
+      level.isCompleted = completedLevels.includes(level.id);
+    }
   }
 
   setCompleted(id) {
